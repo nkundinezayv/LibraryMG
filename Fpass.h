5cbf79568eb5a1f9bd9845e095c66c99c37c4210
nkundinezayv <nkundinezayves.dev@gmail.com>
2023-06-17 18:46:39 +0200

    update

--- Fpass.h
+++ Fpass.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include"DB_CONN.h"
 namespace LibraryMG {
 
 	using namespace System;
@@ -47,6 +47,7 @@ namespace LibraryMG {
 	private: System::Windows::Forms::TextBox^ textBox2;
 	private: System::Windows::Forms::TextBox^ textBox1;
 	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Panel^ panel3;
 
 	private:
 		/// <summary>
@@ -74,6 +75,7 @@ namespace LibraryMG {
 			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->panel3 = (gcnew System::Windows::Forms::Panel());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
@@ -132,14 +134,15 @@ namespace LibraryMG {
 			// 
 			// pictureBox1
 			// 
-			this->pictureBox1->BackColor = System::Drawing::SystemColors::Highlight;
+			this->pictureBox1->BackColor = System::Drawing::SystemColors::HighlightText;
 			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
-			this->pictureBox1->Location = System::Drawing::Point(248, 22);
+			this->pictureBox1->Location = System::Drawing::Point(252, 12);
 			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(24, 22);
+			this->pictureBox1->Size = System::Drawing::Size(20, 20);
 			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
 			this->pictureBox1->TabIndex = 17;
 			this->pictureBox1->TabStop = false;
+			this->pictureBox1->Click += gcnew System::EventHandler(this, &Fpass::pictureBox1_Click);
 			// 
 			// button1
 			// 
@@ -149,6 +152,7 @@ namespace LibraryMG {
 			this->button1->TabIndex = 16;
 			this->button1->Text = L"Reset";
 			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Fpass::button1_Click);
 			// 
 			// textBox3
 			// 
@@ -180,10 +184,19 @@ namespace LibraryMG {
 			this->label1->TabIndex = 12;
 			this->label1->Text = L"Reset Password";
 			// 
+			// panel3
+			// 
+			this->panel3->BackColor = System::Drawing::SystemColors::HotTrack;
+			this->panel3->Location = System::Drawing::Point(-2, 0);
+			this->panel3->Name = L"panel3";
+			this->panel3->Size = System::Drawing::Size(10, 262);
+			this->panel3->TabIndex = 24;
+			// 
 			// Fpass
 			// 
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::None;
 			this->ClientSize = System::Drawing::Size(284, 261);
+			this->Controls->Add(this->panel3);
 			this->Controls->Add(this->panel4);
 			this->Controls->Add(this->panel2);
 			this->Controls->Add(this->panel1);
@@ -198,6 +211,7 @@ namespace LibraryMG {
 			this->Controls->Add(this->label1);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
 			this->Name = L"Fpass";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"Fpass";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
@@ -205,5 +219,38 @@ namespace LibraryMG {
 
 		}
 #pragma endregion
-	};
+	private: System::Void pictureBox1_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->Close();
+	}
+private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+	String^ email = textBox1->Text;
+	String^ hintPassword = textBox2->Text;
+	String^ newPassword = textBox3->Text;
+
+	SqlConnection^ sqlConn = Db_CONN::GetSqlConnection();
+	sqlConn->Open();
+
+	// Check if the email exists and the hint password matches
+	String^ query = String::Format("SELECT pwdhintl FROM users WHERE email = '{0}'", email);
+	SqlCommand^ command = gcnew SqlCommand(query, sqlConn);
+	String^ storedHintPassword = static_cast<String^>(command->ExecuteScalar());
+
+	if (storedHintPassword != nullptr && storedHintPassword == hintPassword)
+	{
+		// Update the password
+		query = String::Format("UPDATE users SET password = '{0}' WHERE email = '{1}'", newPassword, email);
+		command = gcnew SqlCommand(query, sqlConn);
+		command->ExecuteNonQuery();
+		this->Close();
+		MessageBox::Show("Password has been updated successfully.", "Password Updated", MessageBoxButtons::OK, MessageBoxIcon::Information);
+	}
+	else
+	{
+		MessageBox::Show("Invalid email or hint password.", "Error", MessageBoxButtons::OK, MessageBoxIcon::Error);
+	}
+
+	sqlConn->Close();
+
+}
+};
 }
